8efe76ad62d46f05a99ca9e56ae85eb6d3b1c356
Jesus Rios <juniorbump@gmail.com>
2020-07-22 20:07:28 +0900

    added tuner line

--- PitchBender/TunerView.swift
+++ PitchBender/TunerView.swift
@@ -12,6 +12,7 @@ import UIKit
 class TunerView : UIView {
     
     var path: UIBezierPath!
+    var lineView : UIView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,17 +24,23 @@ class TunerView : UIView {
     
     override func draw(_ rect: CGRect) {
         self.createPath()
+        self.addLine()
         
-        UIColor.gray.setFill()
+        UIColor.lightGray.setFill()
         path.fill()
     }
     
     func createPath() {
         let center = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
         path = UIBezierPath(arcCenter: center, radius: frame.size.height/2, startAngle: CGFloat(180.0) * .pi/180, endAngle: CGFloat(0.0) * .pi/180, clockwise: true)
-        
         path.close()
 
+    }
+    
+    func addLine() {
+        lineView = UIView(frame: CGRect(x: frame.size.width/2, y: 5, width: 1, height: frame.size.height/2 - 10))
+        lineView.backgroundColor = .black
+        self.addSubview(lineView)
     }
 
 }
